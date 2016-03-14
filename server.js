9d203c1c23cefd63f066ed818475a396ce846bd1
froster2006 <huaningnie@gmail.com>
2016-03-13 23:16:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,69 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
+var db = mongojs('maiduo', ['maiduo_shop','maiduo_order','maiduo_groupbuy']);
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/contactlist');
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+
+app.get('/shop', function(req,res) {
+  console.log('list all shops');
+  db.maiduo_shop.find(function(err,docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/shop', function(req,res) {
+  console.log("POST: " + req.body);
+  db.maiduo_shop.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+app.get('/shop/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.maiduo_shop.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/shop/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.maiduo_shop.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+/////////////////////////////////////////////////////////////////////////
+app.post('/groupbuy', function(req,res) {
+  console.log("POST: " + req.body);
+  db.maiduo_groupbuy.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/groupbuy', function(req,res) {
+  console.log('list all groupbuy');
+  db.maiduo_groupbuy.find(function(err,docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+})
+
+app.delete('/groupbuy/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.maiduo_ågroupbuy.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+/////////////////////////////////////////////////////////////////////////
 
 app.get('/contactlist', function (req, res) {
   console.log('I received a GET request');
@@ -19,7 +77,7 @@ app.get('/contactlist', function (req, res) {
 });
 
 app.post('/contactlist', function (req, res) {
-  console.log(req.body);
+  console.log("POST: " + req.body);
   db.contactlist.insert(req.body, function(err, doc) {
     res.json(doc);
   });
