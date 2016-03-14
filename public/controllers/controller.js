3e5375bb560db4f4887aae621b61ff1d51093b84
froster2006 <huaningnie@gmail.com>
2016-03-13 23:15:56 -0400

    Update controller.js

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,49 +1,54 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
-
 
 var refresh = function() {
-  $http.get('/contactlist').success(function(response) {
-    console.log("I got the data I requested");
-    $scope.contactlist = response;
-    $scope.contact = "";
+  $http.get('/shop').success(function(response) {
+    $scope.shopList = response;
   });
+  $http.get('/groupbuy').success(function(response) {
+    $scope.groupbuyList = response;
+  });
+  $scope.groupbuy = "";
+  $scope.shop = "";
 };
 
 refresh();
 
-$scope.addContact = function() {
-  console.log($scope.contact);
-  $http.post('/contactlist', $scope.contact).success(function(response) {
+
+$scope.addShop = function() {
+  console.log($scope.shop);
+  $http.post('/shop', $scope.shop).success(function(response) {
     console.log(response);
     refresh();
   });
 };
 
-$scope.remove = function(id) {
+
+
+$scope.removeShop = function(id) {
   console.log(id);
-  $http.delete('/contactlist/' + id).success(function(response) {
+  $http.delete('/shop/' + id).success(function(response) {
     refresh();
   });
 };
 
-$scope.edit = function(id) {
-  console.log(id);
-  $http.get('/contactlist/' + id).success(function(response) {
-    $scope.contact = response;
-  });
-};  
 
-$scope.update = function() {
-  console.log($scope.contact._id);
-  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+$scope.addGroupbuy = function(id) {
+  $scope.groupbuy.shopId = id;
+  $scope.groupbuy.status = "off";
+  $scope.groupbuy.batchId = 1;
+  console.log($scope.groupbuy);
+  $http.post('/groupbuy', $scope.groupbuy).success(function(response) {
+    console.log(response);
     refresh();
-  })
+  });
 };
 
-$scope.deselect = function() {
-  $scope.contact = "";
-}
+$scope.removeGroupbuy = function(id) {
+  console.log(id);
+  $http.delete('/groupbuy/' + id).success(function(response) {
+    refresh();
+  });
+};
 
-}]);﻿
+}]);
