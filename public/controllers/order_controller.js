0762bd0574c6b2a5b78271505e6bfe9e937a86fe
nie.huaning <nie.huaning@validusresearch.com>
2016-03-15 22:27:23 -0400

    fix order ui

--- public/controllers/order_controller.js
+++ public/controllers/order_controller.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngAnimate','ui.bootstrap','ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function($routeProvider, $locationProvider) {
   $routeProvider
